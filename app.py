ad4c3ce1d9e07dc357499f13c0a88478fd6f320e
JishnuAnand123 <114399388+JishnuAnand123@users.noreply.github.com>
2024-03-17 12:29:38 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,24 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/submit', methods=['POST'])
+def submit():
+    name = request.form.get('name')
+    phone_num=request.form.get("phone")
+    email = request.form.get('email')
+    job_role=request.form.get("job")
+    uploaded_file = request.files['file']
+    # Process and store the data/file as needed
+
+    uploaded_file.save("./{}".format(uploaded_file.filename))
+    return "Data submitted successfully. Thanks for your submission! \n We will get back to you soon."
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
+    
